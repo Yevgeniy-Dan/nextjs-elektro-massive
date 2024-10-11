28e1834ec05c12382ac1507b07c613429aab0765
Yevhenii Danoi <geneforapp@gmail.com>
2024-10-11 19:33:05 +0300

    add generate sitemap for cron job

---
+++ run-sitemap.mjs
@@ -0,0 +1,21 @@
+import { register } from "node:module";
+import { pathToFileURL } from "node:url";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+register("ts-node/esm", pathToFileURL("./"));
+import("./src/scripts/generate-sitemap.ts")
+  .then(async (module) => {
+    if (typeof module.generateSitemaps === "function") {
+      await module.generateSitemaps();
+      console.log("Sitemaps generated successfully!");
+    } else {
+      console.error("generateSitemaps function not found in the module");
+    }
+  })
+  .catch((error) => {
+    console.error("Error running the script:", error);
+  });
